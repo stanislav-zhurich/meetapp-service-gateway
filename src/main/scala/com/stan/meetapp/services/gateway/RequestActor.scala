4b041eca4e38b1cc690c6e374b5e8c25c2a413aa
Stanislav Zhurich <stanislav.zhurich@gmail.com>
2016-01-22 23:13:50 +0300

    Akka http introduction

--- src/main/scala/com/stan/meetapp/services/gateway/RequestActor.scala
+++ src/main/scala/com/stan/meetapp/services/gateway/RequestActor.scala
@@ -1,18 +1,12 @@
 package com.stan.meetapp.services.gateway
 
-import scala.concurrent.ExecutionContextExecutor
-import scala.concurrent.duration.DurationInt
-
-import com.typesafe.config.Config
-import com.typesafe.config.ConfigFactory
-
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
+import akka.actor.Actor
+import com.stan.meetapp.services.common.api.ActorCreationSupport
 import akka.http.scaladsl.marshalling.ToResponseMarshallable.apply
 import akka.http.scaladsl.model.HttpEntity.apply
 import akka.http.scaladsl.model.HttpResponse
-import akka.http.scaladsl.model.StatusCodes.Unauthorized
 import akka.http.scaladsl.model.StatusCodes.MethodNotAllowed
+import akka.http.scaladsl.model.StatusCodes.Unauthorized
 import akka.http.scaladsl.server.Directive.addByNameNullaryApply
 import akka.http.scaladsl.server.Directive.addDirectiveApply
 import akka.http.scaladsl.server.Directives.Segment
@@ -25,25 +19,19 @@ import akka.http.scaladsl.server.Directives.path
 import akka.http.scaladsl.server.Directives.post
 import akka.http.scaladsl.server.Directives.segmentStringToPathMatcher
 import akka.http.scaladsl.server.MissingHeaderRejection
-import akka.http.scaladsl.server.Rejection
 import akka.http.scaladsl.server.RejectionHandler
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.RouteResult.route2HandlerFlow
-import akka.stream.ActorMaterializer
-import akka.stream.Materializer
-import akka.util.Timeout
 
 /**
- * @author Stanislav_Zhurich
+ *
+ * @author Stanislav Zhurich
+ *
  */
-object Bootstrap extends App with RestAPIRouter {
+class RequestActor extends Actor with ActorCreationSupport {
 
-  implicit val system = ActorSystem("gateway")
-  implicit val executor = system.dispatcher
-  implicit val materializer = ActorMaterializer()
-  implicit val timeout = new Timeout(5 seconds)
-  val config = ConfigFactory.load()
-
-  Http().bindAndHandle(routes, "0.0.0.0", 8090)
-
+  def receive = {
+    case message => path("test"){
+                        complete("boom")
+                    }
+  }
 }
--- src/main/scala/com/stan/meetapp/services/gateway/RestAPIRouter.scala
+++ src/main/scala/com/stan/meetapp/services/gateway/RestAPIRouter.scala
@@ -1,18 +1,10 @@
 package com.stan.meetapp.services.gateway
 
-import scala.concurrent.ExecutionContextExecutor
-import scala.concurrent.duration.DurationInt
-
-import com.typesafe.config.Config
-import com.typesafe.config.ConfigFactory
-
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshalling.ToResponseMarshallable.apply
 import akka.http.scaladsl.model.HttpEntity.apply
 import akka.http.scaladsl.model.HttpResponse
-import akka.http.scaladsl.model.StatusCodes.Unauthorized
 import akka.http.scaladsl.model.StatusCodes.MethodNotAllowed
+import akka.http.scaladsl.model.StatusCodes.Unauthorized
 import akka.http.scaladsl.server.Directive.addByNameNullaryApply
 import akka.http.scaladsl.server.Directive.addDirectiveApply
 import akka.http.scaladsl.server.Directives.Segment
@@ -25,25 +17,35 @@ import akka.http.scaladsl.server.Directives.path
 import akka.http.scaladsl.server.Directives.post
 import akka.http.scaladsl.server.Directives.segmentStringToPathMatcher
 import akka.http.scaladsl.server.MissingHeaderRejection
-import akka.http.scaladsl.server.Rejection
 import akka.http.scaladsl.server.RejectionHandler
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.RouteResult.route2HandlerFlow
-import akka.stream.ActorMaterializer
-import akka.stream.Materializer
-import akka.util.Timeout
 
 /**
- * @author Stanislav_Zhurich
+ * @author Stanislav Zhurich
  */
-object Bootstrap extends App with RestAPIRouter {
+trait RestAPIRouter {
 
-  implicit val system = ActorSystem("gateway")
-  implicit val executor = system.dispatcher
-  implicit val materializer = ActorMaterializer()
-  implicit val timeout = new Timeout(5 seconds)
-  val config = ConfigFactory.load()
+  val rejectionHandler = RejectionHandler.newBuilder().handle {
+    case MissingHeaderRejection("auth-token") =>
+      complete(HttpResponse(Unauthorized, entity = "No cookies, no service!!!"))
+  }
 
-  Http().bindAndHandle(routes, "0.0.0.0", 8090)
+  val routes: Route =
+    path("authenticate") {
+      post { ctx =>
+        ctx.complete("try to authenticate")
+      } ~
+      complete(HttpResponse(MethodNotAllowed, entity = "Only POST is supported for authentication"))
+    } ~
+    path(Segment) { segment =>
+       handleRejections(rejectionHandler.result().seal) {
+         headerValueByName("auth-token") { token =>
 
+           get { ctx =>
+              ctx.complete("requested" + segment + "domain")
+           }
+         }
+     }
+
+   }
 }
